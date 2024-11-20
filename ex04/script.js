498ff98bec282b22f1ff87e0dab0eea261c46f76
gaia14-it <gaiamaria14n@gmail.com>
2024-11-20 12:01:01 -0500

    I'm sorry, I forgot to commit again

--- ex04/script.js
+++ ex04/script.js
@@ -1,5 +1,6 @@
 function task1() {
-  let myArray = [];
+  let myArray = ["boh", "bah", "beh"];
+  let target = document.querySelector("#task-1-target")
   // This will run when you click the button.
   myArray.forEach((item) => {
     let el = document.createElement("p");
@@ -9,13 +10,26 @@ function task1() {
 }
 
 function task2A() {
-  // Write your code in here
+  let array = document.querySelectorAll("#color-list li")
+  console.log(array)
 }
 
 function task2B() {
-  // Writ your code in here
-}
+  let target = document.querySelector("#name-list")
+  let names = document.querySelectorAll("#name-list > li")
+  console.log(names)
+  names.forEach(item => {
+    let newEl = document.createElement("li");
+    newEl.innerText = "Hi " +item.innerText;
+  target.appendChild(newEl)
+})}
 
 function task3() {
   // Write your code in here
+  let colors = document.querySelectorAll(".item p")
+  console.log(colors)
+
+  colors.forEach(item =>{
+    item.style.backgroundColor = "#" + item.innerText
+  })
 }
