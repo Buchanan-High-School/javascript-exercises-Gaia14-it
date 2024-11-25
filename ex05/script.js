f64d9583f8580ae5430c3cab6b62cdf5c91254a3
gaia14-it <gaiamaria14n@gmail.com>
2024-11-25 12:10:23 -0500

    finished task 3

--- ex05/script.js
+++ ex05/script.js
@@ -30,16 +30,36 @@ const teachers = [
 function task1() {
     // Create your own object assigned to a variable.
     // console.log your object when this function is run.
+    let person = {
+        "name" : "Taylor Alison Swift",
+        "nationality" : "American",
+        "job" : "singer-songwriter",
+        "age" : "34",
+        "height" : "5'10",
+    }
+    console.log(person)
 }
 
 function task2() {
     // Write some HTML to create a card display for a person.
     // When you click the button, the data from the `person`
     // object above should be filled in on the page.
+    let name = document.querySelector("#name");
+    name.innerText = "Mr. Bennett";
+    let subject = document.querySelector("#subject");
+    subject.innerText = "Chemistry"
 }
 
 function task3() {
     // 1. Use the array of teachers to start a loop.
     // 2. In the loop, add a "role" property to each object as they go through the loop
     // 3. Use a template string to add a sentence about each person into the #task3 container.
+    teachers.forEach ((person) => {
+        let template = `The teacher is ${person.name} and he has ${person.experience} years of experience.`
+        console.log(template)
+        let newEl = document.createElement("p")
+        newEl.innerText = template
+        let target = document.querySelector("#task3")
+        target.appendChild(newEl)
+    })
 }
